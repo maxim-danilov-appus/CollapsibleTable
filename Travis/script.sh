2f82afd37b52a144cdd435e29999a52694432205
Robert Nash <robscode@icloud.com>
2017-08-10 14:49:07 +0100

    Attempting to fix travis 1

--- Travis/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-xcodebuild -scheme CollapsibleTable -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.0' test
-xcodebuild -scheme CollapsibleTableDemo -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.0' test
